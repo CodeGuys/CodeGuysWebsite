02e928d3b3f12d85b661f7f6e9062d3345425eaf
John Kayne <johnhoganuke2@gmail.com>
2016-08-11 20:12:10 -0400

    Create 0.cpp

---
+++ scripts/0.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+public int main(char[] *args, int argNum) { // I can't remember if this is the correct number of arguments for a "main" function
+  system("PAUSE");
+  exit(0);
+}
